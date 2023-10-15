f4084955c944e90d51cba148dce595d24d332f4c
Tutoriais por Max Vianna <tutoriaismaxvianna@gmail.com>
2023-10-15 20:41:07 -0300

    MVC - Users

---
+++ tests/Fixture/UsersFixture.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * UsersFixture
+ */
+class UsersFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'created' => '2023-10-15 23:39:17',
+                'modified' => '2023-10-15 23:39:17',
+            ],
+        ];
+        parent::init();
+    }
+}
